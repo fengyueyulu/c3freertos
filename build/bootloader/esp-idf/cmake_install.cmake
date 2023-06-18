6d5888ef9166bdd1f3c8384b548daf90844bec89
fengyueyulu <63699653+fengyueyulu@users.noreply.github.com>
2023-06-18 23:15:06 +0800

    增加流水灯功能

--- build/bootloader/esp-idf/cmake_install.cmake
+++ build/bootloader/esp-idf/cmake_install.cmake
@@ -34,12 +34,12 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "D:/Espressif/tools/xtensa-esp32-elf/esp-2022r1-11.2.0/xtensa-esp32-elf/bin/xtensa-esp32-elf-objdump.exe")
+  set(CMAKE_OBJDUMP "D:/Espressif/tools/riscv32-esp-elf/esp-2022r1-11.2.0/riscv32-esp-elf/bin/riscv32-esp-elf-objdump.exe")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("D:/esppro/led/hello_world/build/bootloader/esp-idf/xtensa/cmake_install.cmake")
+  include("D:/esppro/led/hello_world/build/bootloader/esp-idf/riscv/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
